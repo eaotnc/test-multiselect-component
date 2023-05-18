a206833d62499a29e16ecb20e43e8060aa980a98
ThanatchaGithub <bonsai.eao@gmail.com>
2023-05-18 15:43:53 +0700

    add table select

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Multiselect } from "multiselect-react-dropdown";
+import { TableSelection } from "./TableSelect";
+
+const options = [
+  { name: "Srigar", id: 1 },
+  { name: "Sam", id: 2 },
+  { name: "Sam3", id: 3 },
+  { name: "Sam4", id: 4 },
+];
+const data = [
+  {
+    id: "1",
+    avatar: "",
+    name: "Robert Wolfkisser",
+    email: "rob_wolf@gmail.com",
+    job: "Engineer",
+  },
+
+  {
+    id: "2",
+    avatar: "",
+    name: "Jill Jailbreaker",
+    email: "jj@breaker.com",
+    job: "Engineer",
+  },
+  {
+    id: "3",
+    avatar: "",
+    name: "Henry Silkeater",
+    email: "henry@silkeater.io",
+    job: "Designer",
+  },
+  {
+    id: "4",
+    avatar: "",
+    name: "Bill Horsefighter",
+    email: "bhorsefighter@gmail.com",
+    job: "Designer",
+  },
+  {
+    id: "5",
+    avatar: "",
+    name: "Jeremy Footviewer",
+    email: "jeremy@foot.dev",
+    job: "Manager",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Multiselect
+        showCheckbox={true}
+        options={options} // Options to display in the dropdown
+        selectedValues={(e: any) => console.log(e)} // Preselected value to persist in dropdown
+        onSelect={(e) => console.log(e)} // Function will trigger on select event
+        onRemove={(e) => console.log(e)} // Function will trigger on remove event
+        displayValue="name" // Property name to display in the dropdown options
+      />
+      <TableSelection data={data} />
     </div>
   );
 }
